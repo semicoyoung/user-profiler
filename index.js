9746b3c320a72f633b8aff65314e5712a11a10d5
semicoyoung <eetyang@163.com>
2017-02-12 21:54:38 +0800

    exports as middleware

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var _ = require('lodash');
  * @param mqOptions.queueRegion
  */
 function Extract(mqOptions){
+  let mqOptions = mqOptions || {};
   if (!mqOptions.accountId || !mqOptions.accessKeyId || !mqOptions.accessKeySecret || !mqOptions.queueName || !mqOptions.queueRegion) {
     throw new Error('argument lack');
   }
@@ -17,84 +18,85 @@ function Extract(mqOptions){
   let account = new ALiMns.Account(mqOptions.accountId, mqOptions.accessKeyId, mqOptions.accessKeySecret);
   let mq = new ALiMns.MQ(mqOptions.queueName, account, mqOptions.queueRegion);
   this.mq = mq;
-}
 
-Extract.prototype.extract = function(req) {
-  let params = {};
-  switch (req.method) {
-    case 'GET':
-      params = req.query;
-      break;
-    default:
-      params = req.body;
-      break;
-  }
-  for (let key in req.params) {
-    params[key] = req.params[key];
-  }
+  return function(req, res, next) {
+    let params = {};
+    switch (req.method) {
+      case 'GET':
+        params = req.query;
+        break;
+      default:
+        params = req.body;
+        break;
+    }
 
-  let data = params;
+    for (let key in req.params) {
+      params[key] = req.params[key];
+    }
 
-  let keyMap = {
-    'phone': 'phone',
-    'Phone': 'phone',
-    'sell': 'phone',
-    'mobile': 'phone',
-    'mobilePhone': 'phone',
-    'mobile_phone': 'phone',
-    'email': 'email',
-    'mail': 'email',
-    'user_id': 'user_id',
-    'userId': 'user_id',
-    'cookie_id': 'cookie_id',
-    'cookieId': 'cookie_id',
-    'cookie': 'cookie_id',
-    'user_name': 'user_name',
-    'userName': 'user_name',
-    'name': 'user_name',
-    'ip': 'ip',
-    'IP': 'ip',
-    'age': 'age',
-    'address': 'address',
-    'third': 'third',
-    'source': 'source',
-    'nickname': 'nickname',
-    'birthday': 'birthday',
-    'qq': 'qq',
-    'QQ': 'qq',
-    'qq_number': 'qq',
-    'qqNumber': 'qq',
-    'QQ_number': 'qq',
-    'QQNumber': 'qq',
-    'wechat': 'wechat',
-    'weChat': 'wechat',
-    '微信': 'wechat'
-  };
+    let data = params;
 
-  let extractResult = [];
+    let keyMap = {
+      'phone': 'phone',
+      'Phone': 'phone',
+      'sell': 'phone',
+      'mobile': 'phone',
+      'mobilePhone': 'phone',
+      'mobile_phone': 'phone',
+      'email': 'email',
+      'mail': 'email',
+      'user_id': 'user_id',
+      'userId': 'user_id',
+      'cookie_id': 'cookie_id',
+      'cookieId': 'cookie_id',
+      'cookie': 'cookie_id',
+      'user_name': 'user_name',
+      'userName': 'user_name',
+      'name': 'user_name',
+      'ip': 'ip',
+      'IP': 'ip',
+      'age': 'age',
+      'address': 'address',
+      'third': 'third',
+      'source': 'source',
+      'nickname': 'nickname',
+      'birthday': 'birthday',
+      'qq': 'qq',
+      'QQ': 'qq',
+      'qq_number': 'qq',
+      'qqNumber': 'qq',
+      'QQ_number': 'qq',
+      'QQNumber': 'qq',
+      'wechat': 'wechat',
+      'weChat': 'wechat',
+      '微信': 'wechat'
+    };
 
-  if (data instanceof Array) {
-    for(let i = 0, len = data.length; i < len; i++) {
+    let extractResult = [];
+    if (data instanceof Array) {
+      for(let i = 0, len = data.length; i < len; i++) {
+        let retObj = {};
+        iterData(data[i], keyMap, retObj);
+        if (!_.isEmpty(retObj)) {
+          extractResult.push(retObj);
+        }
+      }
+    } else if (data instanceof Object) {
       let retObj = {};
-      iterData(data[i], keyMap, retObj);
+      iterData(data, keyMap, retObj);
       if (!_.isEmpty(retObj)) {
         extractResult.push(retObj);
       }
     }
-  } else if (data instanceof Object) {
-    let retObj = {};
-    iterData(data, keyMap, retObj);
-    if (!_.isEmpty(retObj)) {
-      extractResult.push(retObj);
-    }
-  }
 
-  this.mq.sendP(JSON.stringify(extractResult)).then(function() {
-    console.log('data===', extractResult);
-  }, function(error) {
-    console.log(error);
-  });
-};
+    this.mq.sendP(JSON.stringify(extractResult)).then(function() {
+      console.log('data===', extractResult);
+    }, function(error) {
+      console.log(error);
+    });
+    next();
+  }
+}
 
 let iterData = function(data, keyMap, retObj) {
   // data is an Array
